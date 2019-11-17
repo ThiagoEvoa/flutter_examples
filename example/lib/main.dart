908d783b84989048f08a63a6ef2a9201c8e45ce9
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-11-17 20:49:01 -0300

    Commit drawer

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:example/page1.dart';
-import 'package:example/page2.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -23,10 +21,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Widget> pages = [Page1(), Page2()];
-
   @override
   Widget build(BuildContext context) {
-    return Material();
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.person,
+                          color: Theme.of(context).primaryColor,
+                          size: 50,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Drawer",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  Text(
+                    "Example",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              selected: false,
+              onTap: () {
+                Scaffold.of(context).openEndDrawer();
+              },
+              leading: Icon(
+                Icons.home,
+                color: Colors.blue,
+              ),
+              title: Text(
+                "Option 1",
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
