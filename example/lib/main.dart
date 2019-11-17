7142a45771289d193ebb7de5db376dc6ee3078d6
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-11-17 16:48:26 -0300

    Commit animatedcontainer

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,18 +21,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double _width = 200;
+  double _height = 200;
+
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          child: FlutterLogo(),
-          decoration: BoxDecoration(
-            color: Colors.blue[50],
-            shape: BoxShape.circle,
-            border: Border.all(color: Colors.blue),
+        child: InkWell(
+          onTap: () {
+            setState(() {
+              _width += 100;
+              _height += 100;
+            });
+          },
+          onDoubleTap: () {
+            setState(() {
+              _width -= 100;
+              _height -= 100;
+            });
+          },
+          child: AnimatedContainer(
+            duration: Duration(seconds: 3),
+            width: _width,
+            height: _height,
+            child: FlutterLogo(),
+            decoration: BoxDecoration(
+              color: Colors.blue[50],
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.blue),
+            ),
           ),
         ),
       ),
