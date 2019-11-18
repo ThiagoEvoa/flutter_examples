8b6c557e1e2570b9b0aaa519bde596d5dbd89e9f
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-11-17 21:02:27 -0300

    Commit snackbar

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:example/menu_item_enum.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -22,30 +21,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _globalKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          PopupMenuButton(
-            icon: Icon(Icons.menu),
-            onSelected: (result) {},
-            itemBuilder: (context) => <PopupMenuEntry<MenuItens>>[
-              const PopupMenuItem<MenuItens>(
-                value: MenuItens.option1,
-                child: Text("First Option"),
-              ),
-              const PopupMenuItem<MenuItens>(
-                value: MenuItens.option2,
-                child: Text("Second Option"),
-              ),
-              const PopupMenuItem<MenuItens>(
-                value: MenuItens.option3,
-                child: Text("Third Option"),
-              ),
-            ],
+      key: _globalKey,
+      appBar: AppBar(),
+      body: Center(
+        child: Builder(
+          builder: (context) => RaisedButton(
+            onPressed: () {
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  action: SnackBarAction(
+                    onPressed: () {},
+                    label: 'ok',
+                    textColor: Colors.white,
+                  ),
+                  content: Text("Snackbar"),
+                  backgroundColor: Colors.blue,
+                  duration: Duration(seconds: 3),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              );
+            },
+            child: Text("Button"),
           ),
-        ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _globalKey.currentState.showSnackBar(
+            SnackBar(
+              action: SnackBarAction(
+                onPressed: () {},
+                label: 'ok',
+                textColor: Colors.white,
+              ),
+              content: Text("Snackbar with global key"),
+              backgroundColor: Colors.blue,
+              duration: Duration(seconds: 3),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
--- example/lib/menu_item_enum.dart
+++
@@ -1,1 +0,0 @@
-enum MenuItens {option1, option2, option3}
