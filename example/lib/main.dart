9d8ebdd5151cf396fe0c1a101311ba470d295bd8
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-11-23 13:42:57 -0300

    Commit packageinfo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,38 +25,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<PackageInfo> _getPackageInfo() async {
+    return await PackageInfo.fromPlatform();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return Material(
+      child: Center(
+        child: FutureBuilder<PackageInfo>(
+          future: _getPackageInfo(),
+          builder: (context, snapshot) {
+            if (snapshot.data != null) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'AppName: ${snapshot.data.appName}',
+                  ),
+                  Text(
+                    'App BuildNumber: ${snapshot.data.buildNumber}',
+                  ),
+                  Text(
+                    'App PackageNumber: ${snapshot.data.packageName}',
+                  ),
+                  Text(
+                    'App Version: ${snapshot.data.version}',
+                  ),
+                ],
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
