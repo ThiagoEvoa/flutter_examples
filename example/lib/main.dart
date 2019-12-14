56ca46531f6fe93dacbcca2fc747eecfbaabfc42
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-12-14 12:19:40 -0300

    Commit cliprrect

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,14 +24,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return Material(
