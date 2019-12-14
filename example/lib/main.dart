6f457fd46c86010381c984044e4702cc542c4af4
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-12-14 13:18:22 -0300

    Commit transform

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -24,19 +26,110 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  double _angle = 0;
+  double _scale = 1;
+  Offset _offset = Offset(0, 0);
+
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Center(
-        child: ClipPath(
-          clipBehavior: Clip.hardEdge,
-          child: Container(
-            width: 300,
-            height: 300,
-            color: Colors.black,
-            child: FlutterLogo(),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Transform.rotate(
+                      angle: _angle,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Transform.scale(
+                      scale: _scale,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Transform.translate(
+                      offset: _offset,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        _angle += pi / 4;
+                      });
+                    },
+                    child: Text('Rotate'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: RaisedButton(
+                      onPressed: () {
+                        setState(() {
+                          _scale += 1;
+                        });
+                      },
+                      child: Text('Scale'),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        _offset = Offset(50, 50);
+                      });
+                    },
+                    child: Text('Translate'),
+                  ),
+                ],
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                setState(() {
+                  _angle = 0;
+                  _scale = 1;
+                  _offset = Offset(0, 0);
+                });
+              },
+              child: Text('Reset'),
+            ),
+          ],
         ),
       ),
     );
