fc8cb9719069a1d30d89a6d7c99a64935063838e
Thiago Agap <thiagoagap@Thiagos-Air.Home>
2020-03-25 11:14:49 +0000

    Commit connectivity

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -62,8 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
