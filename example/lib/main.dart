35c2b89861e9e838581f3a6e2ad8b5568e61611a
Thiago Evoá <thiagoevoa@Thiagos-MBP.Home>
2020-07-10 19:39:55 +0100

    Commit checkbox_listtile

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,38 +24,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final items = List<String>.generate(20, (items) => "CheckboxListTile $items");
+  final itemsCheck = List<bool>.generate(20, (items) => false);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    return Material(
+      child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return CheckboxListTile(
+            value: itemsCheck[index],
+            onChanged: (value) {
+              setState(() {
+                itemsCheck[index] = value;
+              });
+            },
+            title: Text(items[index]),
+            secondary: Icon(Icons.android),
+            controlAffinity: ListTileControlAffinity.platform,
+          );
+        },
       ),
     );
   }
