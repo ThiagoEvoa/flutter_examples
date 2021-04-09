4c3c80e47f51440bdaf49a689c9ad6e695454573
Thiago Evoá <thiagoevoa@Thiagos-MBP-2.Home>
2021-04-09 11:39:42 +0100

    Commit widgets_binding_observer

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,31 @@
-# WidgetsBindingObserver
-<p align="center">
-<img src="https://github.com/ThiagoEvoa/flutter_examples/blob/master/images/widgets_binding_observer.gif" height="649" width="300">
-</p>
+import 'package:flutter/material.dart';
 
-### Main
-```dart
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
 class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   List<String> lifeCycles = [];
 
@@ -48,4 +69,3 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     );
   }
 }
-```
