e7ff17340b1e7db44bb228be2b8b97b4d4462c43
Thiago Evoá <thiagoevoa@Thiagos-MBP-2.Home>
2021-06-20 14:40:22 +0100

    Commit internationalizationv2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,57 +1,4 @@
-# Internationalization v2
-<p align="center">
-<img src="https://github.com/ThiagoEvoa/flutter_examples/blob/master/images/internationalization.png" height="649" width="300">
-</p>
-
-### Dependencies
-
-#### Pubspec.yaml
-```dart
-dependencies:
-  flutter:
-    sdk: flutter
-  flutter_localizations:
-    sdk: flutter
-
-dev_dependencies:
-  flutter_test:
-    sdk: flutter
-  intl: ^0.17.0
-
-flutter:
-  generate: true
-```
-
-### Configuration 
-<p>In the root of the project, you need  to create a folder with the name <b>assets</b> and inside of this folder create a folder with the name <b>translations</b>, now add your <b>.arb</b> files inside of it
-
-### app_en.arb 
-```dart
-{
-    "title":"English version",
-    "phrase":"You have switched the version to english"
-}
-```
-
-### app_pt.arb 
-```dart
-{
-    "title":"Versão português",
-    "phrase":"Você mudou a versão para português"
-}
-```
-
-<p> In the root of the project, create a file with the name <b>l10n.yaml</b> and add the content below
-
-### l10n.yaml
-```dart
-arb-dir: assets/translations
-template-arb-file: app_en.arb
-output-localization-file: app_localizations.dart
-```
-
-### Main
-```dart
+import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -113,4 +60,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-```
